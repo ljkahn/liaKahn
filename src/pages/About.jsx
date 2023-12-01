a9a4e752d6b1a002c9f935b310f82c0781809c8f
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-11-30 18:31:49 -0700

    idk whats happening anymore

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Figure from "react-bootstrap/Figure";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import austynCody from "../assets/workImages/austynCody.png";
